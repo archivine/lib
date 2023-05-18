a5ad066e1f6ab2371e5d477d8d925139f81192d3
vaul <netire24@gmail.com>
2023-05-18 19:45:01 +0200

    Create ex.lua

---
+++ ex.lua
@@ -0,0 +1,12 @@
+local Window = Library:CreateWindow("sheizehax", {AccentColor = Color3.fromRGB(255, 255, 255)});
+
+local MainTab = Window:AddTab("Tab 0")
+
+local LeftGP = MainTab:AddLeftGroupBox("Groupbox 0");
+local Toggle0 = LeftGP:AddToggle("Toggle 0")
+local Slider0 = LeftGP:AddSlider("Slider 0", 0, 100, 25);
+
+
+local RightGP = MainTab:AddRightGroupBox("Groupbox 1");
+local Toggle1 = RightGP:AddToggle("Toggle 1")
+local Slider1 = RightGP:AddSlider("Slider 1", 0, 100, 50);
