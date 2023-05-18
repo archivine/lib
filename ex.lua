7942c40dd477b2f367447a35e6d1a5d45563d0e7
vaul <netire24@gmail.com>
2023-05-18 21:59:23 +0200

    Update ex.lua

--- ex.lua
+++ ex.lua
@@ -7,12 +7,23 @@ local Window = Library:CreateWindow("sheizehax", {
 );
 
 local MainTab = Window:AddTab("Tab 0")
-
 local LeftGP = MainTab:AddLeftGroupBox("Groupbox 0");
-local Toggle0 = LeftGP:AddToggle("Toggle 0")
-local Slider0 = LeftGP:AddSlider("Slider 0", 0, 100, 25);
-
-
 local RightGP = MainTab:AddRightGroupBox("Groupbox 1");
+
+local Toggle0 = LeftGP:AddToggle("Toggle 0") -- Toggle0.State to read value
+Toggle0:OnChanged(function()
+    print(Toggle0.State)
+end)
+
+local Slider0 = LeftGP:AddSlider("Slider 0", 0, 100, { DefaultValue = 25, Rounding = 0 }); --Slider0.CurrentValue to read value
+Slider0:OnChanged(function()
+    print(Slider0.Currentalue)
+end)
+
+
 local Toggle1 = RightGP:AddToggle("Toggle 1")
-local Slider1 = RightGP:AddSlider("Slider 1", 0, 100, 50);
+local Slider1 = LeftGP:AddSlider("Slider 1", 0, 100, { DefaultValue = 50, Rounding = 1 }); 
+
+-- Rounding 0: 1
+-- Rounding 1: 1.1
+-- Rounding 2: 1.11
