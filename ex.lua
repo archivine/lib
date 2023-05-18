e0e0d0d21c2402c0ce2de8d2bf00648c8405dc14
vaul <netire24@gmail.com>
2023-05-18 23:25:17 +0200

    Update ex.lua

--- ex.lua
+++ ex.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/archivine/lib/main/release.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/archivine/lib/main/release.lua"))();
  
 local Window = Library:CreateWindow("sheizehax", {
         AccentColor = Color3.fromRGB(255, 255, 255),
@@ -6,22 +6,22 @@ local Window = Library:CreateWindow("sheizehax", {
     }
 );
 
-local MainTab = Window:AddTab("Tab 0")
+local MainTab = Window:AddTab("Tab 0");
 local LeftGP = MainTab:AddLeftGroupBox("Groupbox 0");
 local RightGP = MainTab:AddRightGroupBox("Groupbox 1");
 
-local Toggle0 = LeftGP:AddToggle("Toggle 0") -- Toggle0.State to read value
+local Toggle0 = LeftGP:AddToggle("Toggle 0"); -- Toggle0.State to read value
 Toggle0:OnChanged(function()
-    print(Toggle0.State)
+    print(Toggle0.State);
 end)
 
 local Slider0 = LeftGP:AddSlider("Slider 0", 0, 100, { DefaultValue = 25, Rounding = 0 }); --Slider0.CurrentValue to read value
 Slider0:OnChanged(function()
-    print(Slider0.Currentalue)
+    print(Slider0.Currentalue);
 end)
 
 
-local Toggle1 = RightGP:AddToggle("Toggle 1")
+local Toggle1 = RightGP:AddToggle("Toggle 1");
 local Slider1 = LeftGP:AddSlider("Slider 1", 0, 100, { DefaultValue = 50, Rounding = 1 }); 
 
 -- Rounding 0: 1
