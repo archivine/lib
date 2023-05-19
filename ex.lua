f4e17d59c6463900731002f87dd8e8c18c32c761
vaul <netire24@gmail.com>
2023-05-19 17:39:27 +0200

    Update ex.lua

--- ex.lua
+++ ex.lua
@@ -1,8 +1,8 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/archivine/lib/main/release.lua"))();
- 
+local Library = loadstring(readfile("release2.lua"))();
+
 local Window = Library:CreateWindow("sheizehax", {
         AccentColor = Color3.fromRGB(255, 255, 255),
-        Size = UDim2.new(600, 700)
+        Size = UDim2.new(0, 600, 0, 700)
     }
 );
 
@@ -17,9 +17,12 @@ end)
 
 local Slider0 = LeftGP:AddSlider("Slider 0", 0, 100, { DefaultValue = 25, Rounding = 0 }); --Slider0.CurrentValue to read value
 Slider0:OnChanged(function()
-    print(Slider0.Currentalue);
+    print(Slider0.CurrentValue);
 end)
 
+local Button0 = RightGP:AddButton("Button 0", function()
+	   print("clicked");
+end);
 
 local Toggle1 = RightGP:AddToggle("Toggle 1");
 local Slider1 = LeftGP:AddSlider("Slider 1", 0, 100, { DefaultValue = 50, Rounding = 1 }); 
